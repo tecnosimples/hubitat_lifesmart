1082c2126b2fd1dea54301dfca3619087592b63f
TecnoSimples Tecnologia LTDA <contato@tecnosimples.com.br>
2026-06-09 15:13:07 -0300

    release: pacote LifeSmart v1.3.0

--- drivers/LifeSmart-Motion-Sensor.groovy
+++ drivers/LifeSmart-Motion-Sensor.groovy
@@ -6,7 +6,7 @@
  * (c) 2026 TecnoSimples - Todos os direitos reservados.
  *
  * Produto licenciado. Distribuido via Hubitat Package Manager.
- * Versao do pacote: 1.2.0
+ * Versao do pacote: 1.3.0
  */
  
 metadata {
